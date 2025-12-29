f7fe047c7fe1fae12b90a1a85f465b79ae28917f
FajarFarel <farelparjo@gmail.com>
2025-12-29 09:21:06 +0700

    plis last3

--- lib/pages/atk/tabel/tabel_data_atk.dart
+++ lib/pages/atk/tabel/tabel_data_atk.dart
@@ -6,7 +6,6 @@ import 'package:gudang_fk/controller/atk/tabel_atk_controller.dart';
 import 'package:gudang_fk/controller/atk/hapus_data_atk_controller.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'dart:io';
 // import 'package:image_picker/image_picker.dart';
 
 class TabelStockAtk extends StatefulWidget {
@@ -21,66 +20,6 @@ class _TabelStockAtkState extends State<TabelStockAtk> {
   final HapusDataAtkController _hapusDataAtkController =
       HapusDataAtkController();
   final EditDataAtkController _editDataAtkController = EditDataAtkController();
-  // final ImagePicker _picker = ImagePicker();
-  // File? _selectedImage;
-
-  // Future<void> _pickImage(ImageSource source) async {
-  //   try {
-  //     final XFile? picked = await _picker.pickImage(
-  //       source: source,
-  //       maxWidth: 1200,
-  //       imageQuality: 80,
-  //     );
-  //     if (picked != null) {
-  //       setState(() {
-  //         _selectedImage = File(picked.path);
-  //       });
-  //     }
-  //   } catch (e) {
-  //     ScaffoldMessenger.of(
-  //       context,
-  //     ).showSnackBar(SnackBar(content: Text('Gagal ambil gambar $e')));
-  //   }
-  // }
-
-  // void _showImageOptions() {
-  //   showModalBottomSheet(
-  //     context: context,
-  //     shape: const RoundedRectangleBorder(
-  //       borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-  //     ),
-  //     builder: (context) => SafeArea(
-  //       child: Wrap(
-  //         children: [
-  //           ListTile(
-  //             leading: const Icon(Icons.camera_alt),
-  //             title: const Text('Ambil Foto'),
-  //             onTap: () {
-  //               Navigator.of(context).pop();
-  //               _pickImage(ImageSource.camera);
-  //             },
-  //           ),
-  //           ListTile(
-  //             leading: const Icon(Icons.photo_library),
-  //             title: const Text('Pilih dari Galeri'),
-  //             onTap: () {
-  //               Navigator.of(context).pop();
-  //               _pickImage(ImageSource.gallery);
-  //             },
-  //           ),
-  //           ListTile(
-  //             leading: const Icon(Icons.close),
-  //             title: const Text('Batal'),
-  //             onTap: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   void initState() {
     super.initState();
